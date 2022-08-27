00a1d33fc3f9aeda0f8ab860e2079634f0e5022f
Luis Martinez <18030545@itcelaya.edu.mx>
2022-08-26 21:33:35 -0500

    the api is complete 100%

--- index.js
+++ index.js
@@ -59,7 +59,10 @@ app.post('/api/shorturl',(req, res)=>{
   const {url} = req.body;
   let newURI = "";
   
-  if (validUrl.isUri(url)){
+  var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+  var regURL = new RegExp(urlRegex, 'i');
+  var urlVerified = url.length < 2083 && regURL.test(url);
+  if (validUrl.isUri(url) && urlVerified){
       let regex = /([A-Za-z]+\.)?(\w+)\.(\w+)/g;
       if(url.match(regex)){
         newURI = url.match(regex).toString();  
